abe8507c1d926f126a4684d22bf98c8bfe079b70
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-04 18:05:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+att_reader :roster
+  def initialize(school)
+    @school = school
+  end
+
+school.roster
+school = School.new("Bayside High School")
+end
+
+
+# school.add_student("Zach Morris", 9)
+# school.add_student("AC Slater", 9)
+# school.add_student("Kelly Kapowski", 10)
+# school.add_student("Screech", 11)
+#
+# school.roster
+# school.grade(9)
+# school.sort
